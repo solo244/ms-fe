fde6457bfaa99030a103e11f67e6d1aaa8af70ed
Solo244 <solo244@gmail.com>
2018-01-29 23:02:58 +0100

    Update dev bundle

--- dev/js/main/main.js
+++ dev/js/main/main.js
@@ -1,16 +1,5 @@
 (function ($) {
-  var path;
 
-  $(".gif").on('click', function() {
-    path = $(this).find("img").attr('src');
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val(path.select());
-    console.log($temp);
-    document.execCommand("copy");
-    $temp.remove();
-  });
-
-  var gifs = Gifffer();
+  new Clipboard('.btn');
 
 })(jQuery);
