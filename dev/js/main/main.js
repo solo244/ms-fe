c6373090ec969d5f5a92d0334ef3c6867251411d
Solo244 <solo244@gmail.com>
2018-01-29 23:40:35 +0100

    Add devs for copy message

--- dev/js/main/main.js
+++ dev/js/main/main.js
@@ -1,5 +1,18 @@
 (function ($) {
 
+  //
   new Clipboard('.btn');
+
+  //
+  $(".gif").on("click", setCopy);
+
+  function setCopy(e) {
+    $(this).addClass("clicked");
+    var setTime = setInterval(function () {
+      $(".gif").removeClass("clicked");
+      clearInterval(setTime);
+      console.log("here");
+    }, 800);
+  }
 
 })(jQuery);
